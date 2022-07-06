3f9db1c65d56bbe5486984a5a1d3fb5c22226133
aushot <marcomaraglino5@gmail.com>
2022-07-06 13:45:50 +0200

    initial

--- src/main/java/me/aushot/Exchpscppbot/JsonFile/ReadFile.java
+++ src/main/java/me/aushot/Exchpscppbot/JsonFile/ReadFile.java
@@ -16,7 +16,7 @@ public class ReadFile extends Utils {
     private Exchangebot exchangebot;
     JSONParser parser = new JSONParser();
 
-    JSONArray a = (JSONArray) parser.parse(new FileReader("changedirectoryhere"));
+    JSONArray a = (JSONArray) parser.parse(new FileReader("changedirectoryhere "));
 
     public ReadFile(Exchangebot exchangebot) throws IOException, ParseException {
         this.exchangebot = exchangebot;
